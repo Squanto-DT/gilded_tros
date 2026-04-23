f2366fbcc201210e4ba04648208e8d32725e6d40
Squanto De Temmerman <sdetemmerman@conxion.be>
2026-04-23 11:51:53 +0100

    clean up code

--- gilded_tros.py
+++ gilded_tros.py
@@ -5,38 +5,6 @@ class GildedTros(object):
     def __init__(self, items):
         self.items = items
 
-#
-#    def update_quality(self):
-#        for item in self.items:
-#            if item.name != "Good Wine" and item.name != "Backstage passes for Re:Factor" \
-#                    and item.name != "Backstage passes for HAXX":
-#                if item.quality > 0:
-#                    if item.name != "B-DAWG Keychain":
-#                        item.quality = item.quality - 1
-#            else:
-#                if item.quality < 50:
-#                    item.quality = item.quality + 1
-#                    if item.name == "Backstage passes for Re:Factor" or item.name == "Backstage passes for HAXX":
-#                        if item.sell_in < 11:
-#                            if item.quality < 50:
-#                                item.quality = item.quality + 1
-#                        if item.sell_in < 6:
-#                            if item.quality < 50:
-#                                item.quality = item.quality + 1
-#            if item.name != "B-DAWG Keychain":
-#                item.sell_in = item.sell_in - 1
-#            if item.sell_in < 0:
-#                if item.name != "Good Wine":
-#                    if item.name != "Backstage passes for Re:Factor" and item.name != "Backstage passes for HAXX":
-#                        if item.quality > 0:
-#                            if item.name != "B-DAWG Keychain":
-#                                item.quality = item.quality - 1
-#                    else:
-#                        item.quality = item.quality - item.quality
-#                else:
-#                    if item.quality < 50:
-#                        item.quality = item.quality + 1
-
     def update_quality(self):
         for item in self.items:
             # Never touch legendary item
@@ -51,8 +19,6 @@ class GildedTros(object):
                 self._update_quality_post_sell_in(item)
 
     def _update_quality_pre_sell_in(self, item):
-        # 'Normal' Item
-        # 'Wine of backstage pass'
         if item.name == 'Good Wine' or 'Backstage passes' in item.name:
             self._increase_quality(item)
             if 'Backstage passes' in item.name and item.sell_in <= 10:
