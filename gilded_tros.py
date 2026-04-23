e79c83d499c04e6411b67ecfc5438a0b1605bb15
Squanto De Temmerman <sdetemmerman@conxion.be>
2026-04-23 10:44:38 +0100

    Never update legendary item

--- gilded_tros.py
+++ gilded_tros.py
@@ -5,38 +5,42 @@ class GildedTros(object):
     def __init__(self, items):
         self.items = items
 
+#
+#    def update_quality(self):
+#        for item in self.items:
+#            if item.name != "Good Wine" and item.name != "Backstage passes for Re:Factor" \
+#                    and item.name != "Backstage passes for HAXX":
+#                if item.quality > 0:
+#                    if item.name != "B-DAWG Keychain":
+#                        item.quality = item.quality - 1
+#            else:
+#                if item.quality < 50:
+#                    item.quality = item.quality + 1
+#                    if item.name == "Backstage passes for Re:Factor" or item.name == "Backstage passes for HAXX":
+#                        if item.sell_in < 11:
+#                            if item.quality < 50:
+#                                item.quality = item.quality + 1
+#                        if item.sell_in < 6:
+#                            if item.quality < 50:
+#                                item.quality = item.quality + 1
+#            if item.name != "B-DAWG Keychain":
+#                item.sell_in = item.sell_in - 1
+#            if item.sell_in < 0:
+#                if item.name != "Good Wine":
+#                    if item.name != "Backstage passes for Re:Factor" and item.name != "Backstage passes for HAXX":
+#                        if item.quality > 0:
+#                            if item.name != "B-DAWG Keychain":
+#                                item.quality = item.quality - 1
+#                    else:
+#                        item.quality = item.quality - item.quality
+#                else:
+#                    if item.quality < 50:
+#                        item.quality = item.quality + 1
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Good Wine" and item.name != "Backstage passes for Re:Factor" \
-                    and item.name != "Backstage passes for HAXX":
-                if item.quality > 0:
-                    if item.name != "B-DAWG Keychain":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes for Re:Factor" or item.name == "Backstage passes for HAXX":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "B-DAWG Keychain":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Good Wine":
-                    if item.name != "Backstage passes for Re:Factor" and item.name != "Backstage passes for HAXX":
-                        if item.quality > 0:
-                            if item.name != "B-DAWG Keychain":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
-
+            if item.name == "B-DAWG Keychain":
+                continue
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
