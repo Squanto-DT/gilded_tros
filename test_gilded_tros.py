586297cee1d0174467f90a1f23b20bd1aa7086d5
Squanto De Temmerman <sdetemmerman@conxion.be>
2026-04-29 19:46:46 +0200

    write unittests

--- test_gilded_tros.py
+++ test_gilded_tros.py
@@ -5,12 +5,73 @@ from gilded_tros import Item, GildedTros
 
 
 class GildedTrosTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_tros = GildedTros(items)
-        gilded_tros.update_quality()
-        self.assertEquals("fixme", items[0].name)
+    
+    def test_normal_item_degrades(self):
+        items = [Item("Normal Item", 10, 20)]
+        tros = GildedTros(items)
+        tros.update_quality()
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(19, items[0].quality)
 
+    def test_normal_item_degrades_twice_after_sell_in(self):
+        items = [Item("Normal Item", -2, 10)]
+        tros = GildedTros(items)
+        tros.update_quality()
+        self.assertEqual(8, items[0].quality)
+
+    def test_legendary_item_never_degrades(self):
+        items = [Item("B-DAWG Keychain", 4, 80)]
+        tros = GildedTros(items)
+        tros.update_quality()
+        self.assertEqual(80, items[0].quality)
+
+    def test_good_wine_increases_in_quality(self):
+        items = [Item(name="Good Wine", sell_in=2, quality=0)]
+        tros = GildedTros(items)
+        tros.update_quality()
+        self.assertEqual(1, items[0].quality)
+
+    def test_good_wine_increases_in_quality_after_sell_in(self):
+        items = [Item(name="Good Wine", sell_in=-2, quality=0)]
+        tros = GildedTros(items)
+        tros.update_quality()
+        self.assertEqual(2, items[0].quality)
+
+    def test_backstage_passes_increase_in_quality_pre_sell_in_more_than_10_days(self):
+        items = [Item(name="Backstage passes for Re:Factor", sell_in=15, quality=20)]
+        tros = GildedTros(items)
+        tros.update_quality()
+        self.assertEqual(21, items[0].quality)
+
+    def test_backstage_passes_increase_in_quality_pre_sell_in_less_than_10_days(self):
+        items = [Item(name="Backstage passes for Re:Factor", sell_in=10, quality=20)]
+        tros = GildedTros(items)
+        tros.update_quality()
+        self.assertEqual(22, items[0].quality)
+
+    def test_backstage_passes_increase_in_quality_pre_sell_in_less_than_5_days(self):
+        items = [Item(name="Backstage passes for Re:Factor", sell_in=5, quality=20)]
+        tros = GildedTros(items)
+        tros.update_quality()
+        self.assertEqual(23, items[0].quality)
+
+    def test_backstage_passes_decrease_to_zero_after_sell_in(self):
+        items = [Item(name="Backstage passes for Re:Factor", sell_in=-2, quality=20)]
+        tros = GildedTros(items)
+        tros.update_quality()
+        self.assertEqual(0, items[0].quality)
+
+    def test_smelly_items_decrease_in_quality_pre_sell_in(self):
+        items = [Item(name="Long Methods", sell_in=3, quality=6)]
+        tros = GildedTros(items)
+        tros.update_quality()
+        self.assertEqual(4, items[0].quality)
+
+    def test_smelly_items_decrease_in_quality_post_sell_in(self):
+        items = [Item(name="Long Methods", sell_in=-1, quality=6)]
+        tros = GildedTros(items)
+        tros.update_quality()
+        self.assertEqual(2, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
